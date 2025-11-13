029b6d9b66caea5233cbf5b41ea4c3eddecef655
Nino <nino.giron13@gmail.com>
2025-11-13 23:13:11 +0100

    ajout d'une fonction de suppression des marqueurs

--- apache-php/src/js/accueil.js
+++ apache-php/src/js/accueil.js
@@ -1,4 +1,4 @@
-Vue.createApp({
+let vue = Vue.createApp({
   data() {
     return{
         map:L.map('map', {
@@ -18,6 +18,32 @@ Vue.createApp({
 
   },
   methods:{
-    
+
   },
-}).mount('#map');
+  beforeMount() {
+   
+  },
+ mounted(){
+  function supression(){
+    event.target.remove()
+  }
+
+  console.log('Initialisation');
+   let icontest1 = L.icon({
+    iconUrl: 'img/camera.png',
+    iconSize: [38, 95],
+    iconAnchor: [22, 94],
+    popupAnchor: [-3, -76],
+    //shadowUrl: 'my-icon-shadow.png',
+    //shadowSize: [68, 95],
+    //shadowAnchor: [22, 94]
+   });
+   let test1 = L.marker([43.9625, 5.7740],{icon: icontest1,alt:'test1'});
+   test1.addTo(this.map).on('click',function(){ supression()});
+   console.log(this.map)
+
+ },
+});
+
+vue.mount('#map');
+
