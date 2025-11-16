4be3df16f44c495fc5ff1601ee55ea8295e507ea
Clément AMIRAULT <clement.amirault@ensg.eu>
2025-11-16 12:07:52 +0100

    Ajout de l'API /objets
    Permet de récupérer les informations de tous les objets au démarrage (/objets), ou d'un objet spécifique (/objets/id?=N).

--- apache-php/src/index.php
+++ apache-php/src/index.php
@@ -8,6 +8,32 @@ Flight::route('/', function() {
     Flight::render('accueil');
 });
 
+
+Flight::route('/objets', function () {
+    $host = 'db';
+    $port = 5432;
+    $dbname = 'mydb';
+    $user = 'postgres';
+    $pass = 'postgres';
+
+    // Connexion BDD
+    $link = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$pass");
+
+    $sql = "SELECT o.*, i.url, i.taille_x, i.taille_y FROM objet o JOIN icone i ON o.id_icone = i.id";
+
+    if ( ! isset($_GET['id']) ) {
+        $sql = "SELECT o.*, i.url, i.taille_x, i.taille_y FROM objet o JOIN icone i ON o.id_icone = i.id";
+        $query = pg_query($link, $sql);
+    } else {
+        $id_objet = $_GET['id'];
+        $sql = "SELECT o.*, i.url, i.taille_x, i.taille_y FROM objet o JOIN icone i ON o.id_icone = i.id WHERE o.id = $1";
+        $query = pg_query_params($link, $sql, [ $id_objet ]); // protection contre injection SQL
+    }
+    $results = pg_fetch_all($query);
+    Flight::json($results);
+});
+
+
 Flight::route('/test-db', function () {
     $host = 'db';
     $port = 5432;
