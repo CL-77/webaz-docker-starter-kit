59920c39507b7627e8e746d6597685f2a7991d99
Clément AMIRAULT <clement.amirault@ensg.eu>
2025-11-22 11:39:12 +0100

    Modification BDD (table score + ajout code PC)

--- db/init.sql
+++ db/init.sql
@@ -10,7 +10,8 @@ CREATE TABLE objet (
     depart bool,
     lat float,
     lon float,
-    code integer,
+    code_bloquant integer,
+    code_revele integer,
     indice character varying,
     id_icone integer,
     id_bloque integer
@@ -24,18 +25,25 @@ CREATE TABLE icone (
     taille_y integer
 );
 
+CREATE TABLE score (
+    id integer GENERATED ALWAYS AS IDENTITY,
+    nom character varying,
+    score integer,
+    classement integer
+);
+
 -- Insertion des données
-INSERT INTO objet (nom, geom, min_zoom_visible, depart, lat, lon, code, indice, id_icone, id_bloque) VALUES
-    ('tacheometre1', ST_SetSRID(ST_MakePoint(5.774149, 43.962906), 4326), 19, true, 43.962906, 5.774149, NULL, NULL, 1, NULL),
-    ('tacheometre2', ST_SetSRID(ST_MakePoint(5.908676, 43.984754), 4326), 19, true, 43.984754, 5.908676, NULL, NULL, 1, NULL),
-    ('tacheometre3', ST_SetSRID(ST_MakePoint(5.791172, 43.961542), 4326), 19, true, 43.961542, 5.791172, NULL, NULL, 1, NULL),
-    ('gnss', ST_SetSRID(ST_MakePoint(5.772059, 43.978721), 4326), 19, true, 43.978721, 5.772059, NULL, NULL, 2, NULL),
-    ('mire', ST_SetSRID(ST_MakePoint(5.909462, 43.983155), 4326), 19, true, 43.983155, 5.909462, NULL, NULL, 3, NULL),
-    ('camera', ST_SetSRID(ST_MakePoint(5.794683, 43.933778), 4326), 19, false, 43.933778, 5.794683, NULL, NULL, 4, NULL),
-    ('carte', ST_SetSRID(ST_MakePoint(5.782106, 44.112433), 4326), 19, true, 44.112433, 5.782106, 2425, NULL, 5, NULL),
-    ('pc', ST_SetSRID(ST_MakePoint(5.773961, 43.962488), 4326), 19, true, 43.962488, 5.773961, NULL, NULL, 6, 9),
-    ('cle', ST_SetSRID(ST_MakePoint(5.782126, 43.957144), 4326), 19, false, 43.957144, 5.782126, NULL, NULL, 7, 10),
-    ('porte', ST_SetSRID(ST_MakePoint(5.794508, 43.933737), 4326), 19, true, 43.933737, 5.794508, NULL, 'Porte fermée à clé !', 8, NULL)
+INSERT INTO objet (nom, geom, min_zoom_visible, depart, lat, lon, code_bloquant, code_revele, indice, id_icone, id_bloque) VALUES
+    ('tacheometre1', ST_SetSRID(ST_MakePoint(5.774149, 43.962906), 4326), 19, true, 43.962906, 5.774149, NULL, NULL, NULL, 1, NULL),
+    ('tacheometre2', ST_SetSRID(ST_MakePoint(5.908676, 43.984754), 4326), 19, true, 43.984754, 5.908676, NULL, NULL, NULL, 1, NULL),
+    ('tacheometre3', ST_SetSRID(ST_MakePoint(5.791172, 43.961542), 4326), 19, true, 43.961542, 5.791172, NULL, NULL, NULL, 1, NULL),
+    ('gnss', ST_SetSRID(ST_MakePoint(5.772059, 43.978721), 4326), 19, true, 43.978721, 5.772059, NULL, NULL, NULL, 2, NULL),
+    ('mire', ST_SetSRID(ST_MakePoint(5.909462, 43.983155), 4326), 19, true, 43.983155, 5.909462, NULL, NULL, NULL, 3, NULL),
+    ('camera', ST_SetSRID(ST_MakePoint(5.794683, 43.933778), 4326), 19, false, 43.933778, 5.794683, NULL, NULL, NULL, 4, NULL),
+    ('carte', ST_SetSRID(ST_MakePoint(5.782106, 44.112433), 4326), 19, true, 44.112433, 5.782106, NULL, 2425, NULL, 5, NULL),
+    ('pc', ST_SetSRID(ST_MakePoint(5.773961, 43.962488), 4326), 19, true, 43.962488, 5.773961, 2425, NULL, NULL, 6, 9),
+    ('cle', ST_SetSRID(ST_MakePoint(5.782126, 43.957144), 4326), 19, false, 43.957144, 5.782126, NULL, NULL, NULL, 7, 10),
+    ('porte', ST_SetSRID(ST_MakePoint(5.794508, 43.933737), 4326), 19, true, 43.933737, 5.794508, NULL, NULL, 'Porte fermée à clé !', 8, 6)
 ;
 
 INSERT INTO icone (nom, url, taille_x, taille_y) VALUES
@@ -67,3 +75,6 @@ ALTER TABLE IF EXISTS public.objet
     REFERENCES public.objet (id) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE NO ACTION;
+
+ALTER TABLE IF EXISTS public.score
+    ADD CONSTRAINT score_pkey PRIMARY KEY (id);
